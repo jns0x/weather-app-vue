846abd801401911339b942a35397b91691502686
Pawel Janus <paweljj@gmail.com>
2018-09-20 00:17:14 +0200

    button

---
+++ src/atoms/RoundButton.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="btn" v-on:click="changeClass" v-bind:class="{'toggled': btnToggle, 'unToggled': btnRemove}">
+    <div class="btn__cross" v-bind:class="{'toggled': btnToggle}"></div>
+
+  </div>
+
+</template>
+<script>
+export default {
+  name: "RoundButton",
+  data() {
+    return {
+      btnToggle: false,
+      btnRemove: true
+    };
+  },
+  methods: {
+    handleClick() {},
+    changeClass() {
+      console.log("test");
+      this.btnToggle = !this.btnToggle;
+      this.btnRemove = !this.btnRemove;
+    }
+  }
+};
+</script>
+<style lang="scss">
+.btn {
+  border: 0.3rem solid white;
+  border-radius: 50%;
+  width: 2.5rem;
+  height: 2.5rem;
+  position: relative;
+  cursor: pointer;
+
+  &:hover {
+    .btn__cross {
+      transform: rotate(180deg);
+      transition: 250ms all;
+    }
+  }
+  &.toggled {
+    animation: popIn 250ms ease-in-out forwards;
+  }
+  &.unToggled {
+    animation: popOut 250ms ease-in-out forwards;
+  }
+  .btn__cross {
+    border: 0.2rem solid white;
+    background: white;
+    border-radius: 1rem;
+    width: 1.6rem;
+    position: absolute;
+    top: 1rem;
+    left: 0.25rem;
+    transition: 250ms all;
+    &:after {
+      content: "";
+      border: 0.2rem solid white;
+      background: white;
+      border-radius: 1rem;
+      width: 1.6rem;
+      position: absolute;
+      transform: rotate(90deg);
+      left: -0.2rem;
+      top: -0.2rem;
+      transition: 250ms all;
+    }
+    &.toggled {
+      &:after {
+        transform: rotate(180deg);
+        transition: 250ms all;
+      }
+    }
+  }
+  @keyframes popIn {
+    0% {
+      transform: scale(1);
+    }
+    20% {
+      transform: scale(0.8);
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
+  @keyframes popOut {
+    0% {
+      transform: scale(1);
+    }
+    20% {
+      transform: scale(0.8);
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
+}
+</style>
+
+
--- src/components/WeatherBox.vue
+++ src/components/WeatherBox.vue
@@ -1,23 +1,29 @@
 <template>
-  <div class="weather-box">
-    <div class="weather__city">{{ weather.name }}</div>
-    <div class="group">
-      <div class="weather__temp">{{ weather.main.temp }}
-        <sup class="unit">o</sup>
+  <div class="weather-box__wrapper">
+    <div class="weather-box">
+      <div class="weather__city">{{ weather.name }}</div>
+      <div class="group">
+        <div class="weather__temp">{{ weather.main.temp }}
+          <sup class="unit">o</sup>
+        </div>
+        <img class="icon" v-bind:src="`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`">
       </div>
-      <img class="icon" v-bind:src="`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`">
+      <p class="weather-desc">{{ weather.weather[0].description }}</p>
+      <p class="weather-desc">humidity: {{ weather.main.humidity }}</p>
+      <p class="weather-desc">pressure: {{ weather.main.pressure }} hpa</p>
+      <p class="weather-desc">wind: {{ weather.wind.speed }} km/h</p>
     </div>
-    <p class="weather-desc">{{ weather.weather[0].description }}</p>
-    <p class="weather-desc">humidity: {{ weather.main.humidity }}</p>
-    <p class="weather-desc">pressure: {{ weather.main.pressure }} hpa</p>
-    <p class="weather-desc">wind: {{ weather.wind.speed }} km/h</p>
-
+    <round-button/>
   </div>
 </template>
 
 <script>
+import RoundButton from "../atoms/RoundButton";
 export default {
   name: "WeatherBox",
+  components: {
+    RoundButton
+  },
   props: {
     weather: {
       type: Object
@@ -32,30 +38,36 @@ export default {
 @import "../styles/variables";
 @import "../styles/mixins";
 
-.weather-box {
-  margin-top: 2rem;
-  // border: 1px solid white;
-  width: 100%;
-  height: auto;
-  .weather__city {
-    @include font-size(1.5);
-    margin: 0.5rem;
-  }
-  .group {
-    display: flex;
-    justify-content: center;
-    .weather__temp {
-      @include font-size(3.5);
-      position: relative;
-      .unit {
-        position: relative;
-        top: -1rem;
-        left: -1rem;
-      }
+.weather-box__wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  .weather-box {
+    margin-top: 2rem;
+    // border: 1px solid white;
+    width: 100%;
+    height: auto;
+    .weather__city {
+      @include font-size(1.5);
+      margin: 0.5rem;
     }
-    .icon {
-      width: 80px;
-      height: 80px;
+    .group {
+      display: flex;
+      justify-content: center;
+      .weather__temp {
+        @include font-size(3.5);
+        position: relative;
+        .unit {
+          position: relative;
+          top: -1rem;
+          left: -1rem;
+        }
+      }
+      .icon {
+        width: 80px;
+        height: 80px;
+      }
     }
   }
 }
