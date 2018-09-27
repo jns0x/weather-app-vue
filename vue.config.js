58df2c95c1ad62b512194b2fbfbc9c434cd5faa4
Pawel Janus <paweljj@gmail.com>
2018-09-27 21:29:39 +0200

    vue config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  baseUrl: "/weather-app-vue-vuex/"
+  baseUrl: "/weather-app-vue-vuex/",
+  outputDir: "dist",
+  assetsDir: "",
+  indexPath: "index.html",
+  pages: {
+    index: {
+      entry: "src/index/main.js"
+    }
+  }
 };
